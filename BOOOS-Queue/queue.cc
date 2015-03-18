90072573732f2e9160cafd0383333a818ec3a52c
Fernando Jorge Mota <f.j.mota13@gmail.com>
2015-03-17 22:38:40 -0300

    Add BOOOS queues

---
+++ BOOOS-Queue/queue.cc
@@ -0,0 +1,48 @@
+#include "queue.h"
+#include <stdlib.h>
+
+using namespace BOOOS;
+
+Queue::Queue() {
+  this->_length = 0;
+  this->head()->next(this->head());
+  this->head()->prev(this->head());
+}
+
+Queue::~Queue() {
+	while (this->_length) {
+		this->remove();
+	}
+}
+
+void Queue::insert(Queue::Element * elem) {
+	Queue::Element *h = this->head();
+	if (this->_length) {
+		Queue::Element *hprevious = h->prev();
+		hprevious->next(elem);
+		elem->next(h->next());
+		elem->prev(hprevious);
+		h->prev(elem);
+	} else {
+		h->next(elem);
+		h->prev(elem);
+		elem->next(elem);
+		elem->prev(elem);
+	}
+	this->_length++;
+}
+
+Queue::Element * Queue::remove() {
+	if (this->_length == 0) {
+		throw "Cannot remove from the file";
+	}
+	Queue::Element *h = this->head();
+	Queue::Element *result = h->next();
+	--this->_length;
+	h->next(result->next());
+	if (this->_length) {
+		h->prev()->next(result->next());
+		result->next()->prev(h->prev());
+	}
+	return result;
+}
